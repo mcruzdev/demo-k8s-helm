1c30dc2b7a0187aad913f22c79f2231c80e55a7d
mcruzdev <matheuscruz.dev@gmail.com>
2022-06-27 09:54:24 -0300

    feat: add prometheus tag

---
+++ src/main/kotlin/com/mcruzdev/k8s/demo/Config.kt
@@ -0,0 +1,17 @@
+package com.mcruzdev.k8s.demo
+
+import io.micrometer.core.instrument.MeterRegistry
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class Config {
+
+    @Bean
+    fun metricsCommonTags(): MeterRegistryCustomizer<MeterRegistry>? {
+        return MeterRegistryCustomizer { registry: MeterRegistry ->
+            registry.config().commonTags("application", "demo")
+        }
+    }
+}
